befbffdf535de366b81213a484958f3257cba437
tengsk <1315575653@qq.com>
2022-05-31 12:06:43 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
+import time
 # browser = webdriver.Firefox()
 # browser.get('http://localhost:8000')
 # assert 'Django' in browser.title
@@ -11,7 +13,25 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do',self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+
         #[...rest of comments as before]
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys('Buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        tabel = self.browser.find_element_by_id('id_list_table')
+        rows = tabel.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1:Buy peacock feathers'for row in rows)
+        )
+        self.fail('Finish the test!')
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
