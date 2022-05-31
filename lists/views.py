aa7c080ecd3df89c38c30ab1e771a5e4cc9157af
tengsk <1315575653@qq.com>
2022-05-31 11:39:58 +0800

    First unit test and url mapping, dummy view

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.test import TestCase
 
-# Create your views here.
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1+1,3)
+# # Create your views here.
+# class SmokeTest(TestCase):
+#     def test_bad_math(self):
+#         self.assertEqual(1+1,3)
+#
+#
+def home_page():
+    pass
+#home_page = None
